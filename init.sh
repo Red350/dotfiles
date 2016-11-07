99e2b08430320afced78ad65d23020f63934d83f
Padraig <pwredmond@gmail.com>
2016-11-07 11:20:44 +0000

    Added initialisation script

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Lazy script to initialise dotfiles.
+# Doesn't check if files exist first, but won't overwrite them anyway.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -s $DIR/.vimrc ~/.vimrc 
+ln -s $DIR/.inputrc ~/.inputrc
